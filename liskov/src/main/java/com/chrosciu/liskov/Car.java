a096f39e9fae8824dacd9b49d2995be193e1fdda
Marcin Chrost <marcin.chrost@contractors.roche.com>
2020-07-09 22:27:04 +0200

    Liskov solution with delegate - Lombok version

--- liskov/src/main/java/com/chrosciu/liskov/Car.java
+++ liskov/src/main/java/com/chrosciu/liskov/Car.java
@@ -3,27 +3,8 @@ package com.chrosciu.liskov;
 import lombok.experimental.Delegate;
 
 public class Car implements Driveable {
+    @Delegate
     private final BaseDriveable baseDriveable = new BaseDriveable(this::driveWithReport);
-
-    @Override
-    public void startEngine() {
-        baseDriveable.startEngine();
-    }
-
-    @Override
-    public void drive(int km) {
-        baseDriveable.drive(km);
-    }
-
-    @Override
-    public void stopEngine() {
-        baseDriveable.stopEngine();
-    }
-
-    @Override
-    public void reportEngineState() {
-        baseDriveable.reportEngineState();
-    }
 
     private void driveWithReport(int km) {
         System.out.println("Driving for: " + km + " km");
--- liskov/src/main/java/com/chrosciu/liskov/TeslaCar.java
+++ liskov/src/main/java/com/chrosciu/liskov/TeslaCar.java
@@ -3,27 +3,8 @@ package com.chrosciu.liskov;
 import lombok.experimental.Delegate;
 
 public class TeslaCar implements Driveable {
+    @Delegate
     private final BaseDriveable baseDriveable = new BaseDriveable(this::driveWithReport);
-
-    @Override
-    public void startEngine() {
-        baseDriveable.startEngine();
-    }
-
-    @Override
-    public void drive(int km) {
-        baseDriveable.drive(km);
-    }
-
-    @Override
-    public void stopEngine() {
-        baseDriveable.stopEngine();
-    }
-
-    @Override
-    public void reportEngineState() {
-        baseDriveable.reportEngineState();
-    }
 
     private void driveWithReport(int km) {
         if (Weather.getInstance().getTemperature() < 0) {
