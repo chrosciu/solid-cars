1e1424f97acf1759931221aabcd28a06fcd8a960
Marcin Chrost <marcin.chrost@contractors.roche.com>
2020-07-10 12:25:30 +0200

    Fixed typo

--- sid/src/main/java/com/chrosciu/sid/Main.java
+++ sid/src/main/java/com/chrosciu/sid/Main.java
@@ -3,7 +3,7 @@ package com.chrosciu.sid;
 public class Main {
     public static void main(String[] args) {
         LiquidChecking liquidChecking = new LiquidCheck();
-        Washing washing = new VaxCarWash();
+        Washing washing = new WaxCarWash();
         Vacuuming vacuuming = new Vacuum();
         CarCare carCare = new CarCare(liquidChecking, washing, vacuuming);
         carCare.care();
